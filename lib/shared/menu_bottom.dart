b079ae2b7a8ceb09ff8b700abc92a1ded36b89d4
Koh Eugene <kyg030320@gmail.com>
2025-04-17 23:10:58 +0900

    Screen3 & menu_bottom

--- lib/shared/menu_bottom.dart
+++ lib/shared/menu_bottom.dart
@@ -1,2 +1,20 @@
 
 // 하단 메뉴바
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MenuBottom extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar (
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: '홈'),
+        BottomNavigationBarItem(icon: Icon(Icons.search), label: '검색'),
+        BottomNavigationBarItem(icon: Icon(Icons.book), label: '서재'),
+        BottomNavigationBarItem(icon: Icon(Icons.perm_identity_rounded), label: '내 프로필')
+      ],
+    );
+  }
+
+}
