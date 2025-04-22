f7816ecd4981454be1204ab4bf0b8f3c445742e3
Koh Eugene <kyg030320@gmail.com>
2025-04-23 00:07:24 +0900

    Make Chatlist/Badge Screen

--- lib/aichat/chatlist_screen.dart
+++ lib/aichat/chatlist_screen.dart
@@ -3,7 +3,8 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:wanbook/shared/menu_bottom.dart';
+
+import '../shared/size_config.dart';
 
 class ChatlistScreen extends StatefulWidget {
   const ChatlistScreen({super.key});
@@ -25,6 +26,69 @@ class _ChatlistScreenState extends State<ChatlistScreen> {
           icon: Icon(Icons.chevron_left_rounded),
           color: Colors.black,
         ),
+      ),
+      body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: SizeConfig.screenWidth*0.05,
+            ),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 16,),
+                  chatRecord('데미안', '데미안의 주제 좀 알려줘', '오후 11:40'),
+                  chatRecord('책 제목', '(제일 최근 물어본 질문)', '오전 9:30')
+                ],
+              ),
+            )
+          )
+      ),
+    );
+  }
+
+  Widget chatRecord(String title, String chat, String time) {
+    return SizedBox(
+      height: 80,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: 50, height: 50,
+            decoration: BoxDecoration(
+                color: Color(0xffD9D9D9),
+                borderRadius: BorderRadius.circular(15)
+            ),
+          ),
+          SizedBox(width: 16,),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 5,),
+              Text(title,
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 15
+                ),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,  // 책 제목이 긴 경우 말줄임표 이용해 한 줄만 출력
+              ),
+              SizedBox(height: 5,),
+              Text(chat, style: TextStyle(
+                color: Color(0xffADADAD),
+                fontWeight: FontWeight.w400,
+                fontSize: 12),
+              )
+            ],
+          ),
+          Spacer(),
+          Text(time, style: TextStyle(
+              color: Color(0xffADADAD),
+              fontWeight: FontWeight.w400,
+              fontSize: 10),
+          )
+        ],
       ),
     );
   }
--- lib/profile/badge_screen.dart
+++ lib/profile/badge_screen.dart
@@ -3,7 +3,8 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:wanbook/profile/profile_screen.dart';
+
+import '../shared/size_config.dart';
 
 class BadgeScreen extends StatefulWidget {
   const BadgeScreen({super.key});
@@ -13,6 +14,10 @@ class BadgeScreen extends StatefulWidget {
 }
 
 class _BadgeScreenState extends State<BadgeScreen> {
+
+  final List<bool> monthlyrecords = List.generate(12, (index) => index < 3,);
+  final List<bool> achievements = List.generate(12, (index) => index < 3);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +30,138 @@ class _BadgeScreenState extends State<BadgeScreen> {
           icon: Icon(Icons.chevron_left_rounded),
           color: Colors.black,
         ),
-      )
+      ),
+      body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: SizeConfig.screenWidth*0.05,
+            ),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(height: 16,),
+                  allMonthlyRecord(),
+                  SizedBox(height: 16,),
+                  allAchieveBadge(achievements),
+                  SizedBox(height: 16,),
+                ],
+              ),
+            ),
+          )
+      ),
+    );
+  }
+
+  Widget recordBadge(String title, bool isLocked) {
+    return SizedBox(
+      width: 65,
+      height: 93,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      color: Color(0xffD9D9D9),
+                      shape: BoxShape.circle),
+                ),
+                if (isLocked) ...[
+                  Positioned(
+                      child: Icon(
+                          Icons.lock,
+                          color: Color(0xff777777),
+                          size: 24
+                      )
+                  )
+                ],
+              ]
+          ),
+          SizedBox(height: 8,),
+          Text(title, style: TextStyle(
+              color: Color(0xff777777),
+              fontWeight: FontWeight.w400,
+              fontSize: 14),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget allMonthlyRecord() {
+    return Container(
+      width: SizeConfig.screenWidth * 0.9,
+      height: 420,
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+          color: Color(0xffF8F8F8),
+          borderRadius: BorderRadius.circular(16)
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('월간 기록', style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w600,
+              fontSize: 18),
+          ),
+          SizedBox(height: 16,),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: GridView.count(
+              crossAxisCount: 4,
+              mainAxisSpacing: 30,
+              crossAxisSpacing: 16,
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              children:
+              List.generate(12, (index) {
+                return recordBadge("${index + 1}월", !monthlyrecords[index]);
+              }),
+            )
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget allAchieveBadge(List<bool> achievedList) {
+    return Container(
+      width: SizeConfig.screenWidth * 0.9,
+      height: 420,
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+          color: Color(0xffF8F8F8),
+          borderRadius: BorderRadius.circular(16)
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('업적 배지', style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w600,
+              fontSize: 18),
+          ),
+          SizedBox(height: 16,),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: GridView.count(
+              crossAxisCount: 4,
+              mainAxisSpacing: 30,
+              crossAxisSpacing: 16,
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              children:
+                List.generate(12, (index) {
+                  return recordBadge("업적 ${index + 1}", !achievedList[index]);
+                }),
+            )
+          )
+        ],
+      ),
     );
   }
 }
