2180c5f0d3624a53cfc07c58a8968026d3215ea5
Koh Eugene <kyg030320@gmail.com>
2025-04-26 20:19:37 +0900

    login 화면 수정

--- lib/login/login_screen.dart
+++ lib/login/login_screen.dart
@@ -17,8 +17,16 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
 
+  final _formKey = GlobalKey<FormState>();
+  final _idController = TextEditingController();
+  final _pwdController = TextEditingController();
+
   // 아이디 저장 여부
   bool saveId = false;
+
+  // 에러 메시지
+  String? _idError;
+  String? _pwdError;
 
   @override
   Widget build(BuildContext context) {
@@ -38,122 +46,207 @@ class _LoginScreenState extends State<LoginScreen> {
             children: [
               Text('완북', style: TextStyle(fontWeight: FontWeight.w600, fontSize: 22, color: Colors.black)),
               SizedBox(height: 24),
-              Container(
-                width: SizeConfig.screenWidth * 0.9,
-                height: 54,
-                decoration: ShapeDecoration(
-                    color: Color(0xffF8F8F8),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16))
-                ),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                      border: InputBorder.none,
-                      contentPadding: EdgeInsets.all(16),
-                      hintText: '아이디를 입력해 주세요',
-                      hintStyle: TextStyle(
-                          color: Color(0xff777777),
-                          fontWeight: FontWeight.w400,
-                          fontSize: 16
-                      )
-                  ),
+              Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    loginField(),
+                    SizedBox(height: 8),
+                    passwordField()
+                  ],
                 ),
               ),
               SizedBox(height: 8),
-              Container(
-                width: SizeConfig.screenWidth * 0.9,
-                height: 54,
-                decoration: ShapeDecoration(
-                    color: Color(0xffF8F8F8),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16))
-                ),
-                child: TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      border: InputBorder.none,
-                      contentPadding: EdgeInsets.all(16),
-                      hintText: '비밀번호를 입력해 주세요',
-                      hintStyle: TextStyle(
-                          color: Color(0xff777777),
-                          fontWeight: FontWeight.w400,
-                          fontSize: 16
-                      )
-                  ),
-                ),
-              ),
+              loginButton(),
               SizedBox(height: 8),
-              SizedBox(
-                width: SizeConfig.screenWidth * 0.9,
-                height: 57,
-                child: TextButton(onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return MenuBottom();
-                  },));
-                },
-                    style: TextButton.styleFrom(
-                        backgroundColor: Color(0xffCCE4FF),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16)
-                        )
-                    ),
-                    child: Text('로그인', style: TextStyle(
-                        color: Color(0xff0077FF),
-                        fontWeight: FontWeight.w600,
-                        fontSize: 18
-                    ),
-                    )
-                )
-              ),
-              SizedBox(height: 8),
-              Row(
-                children: [
-                  Checkbox(
-                    activeColor: Colors.transparent,
-                    checkColor: Color(0xff777777),
-                    value: saveId,
-                    onChanged: (value) {
-                      setState(() {
-                        saveId = value!;
-                      });
-                      },
-                  ),
-                  Text('아이디 저장', style: TextStyle(
-                      fontWeight: FontWeight.w400,
-                      fontSize: 14,
-                      color: Color(0xff777777)
-                  )
-                  )
-                ],
-              ),
+              checkSaveId(),
               Spacer(),
               Divider(thickness: 1, color: Color(0xffC9C9C9)),
               SizedBox(height: 20),
-              SizedBox(
-                  width: SizeConfig.screenWidth * 0.9,
-                  height: 57,
-                  child: TextButton(onPressed: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context) {
-                      return JoinScreen();
-                    },));
-                  },
-                      style: TextButton.styleFrom(
-                          backgroundColor: Color(0xffE4E4E4),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(16)
-                          )
-                      ),
-                      child: Text('회원가입', style: TextStyle(
-                          color: Color(0xff777777),
-                          fontWeight: FontWeight.w600,
-                          fontSize: 18),
-                      )
-                  )
-              ),
+              joinButton(),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  // 아이디 입력 칸
+  Widget loginField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          width: SizeConfig.screenWidth * 0.9,
+          height: 54,
+          decoration: ShapeDecoration(
+              color: Color(0xffF8F8F8),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16))
+          ),
+          child: TextFormField(
+            controller: _idController,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.all(16),
+                hintText: '아이디를 입력해 주세요',
+                hintStyle: TextStyle(
+                    color: Color(0xff777777),
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16
+                ),
+            ),
+          ),
+        ),
+        if (_idError != null)
+          Padding(
+              padding: EdgeInsets.only(left: 8, top: 4),
+              child: Row(
+                children: [
+                  Icon(Icons.error_outline_rounded, size: 12, color: Color(0xffFF4F4F),),
+                  SizedBox(width: 2,),
+                  Text(_idError!, style: TextStyle(
+                    color: Color(0xffFF4F4F),
+                    fontWeight: FontWeight.w600,
+                    fontSize: 12),
+                  ),
+                ],
+              ),
+          )
+      ],
+    );
+  }
+
+  // 비밀번호 입력 칸
+  Widget passwordField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          width: SizeConfig.screenWidth * 0.9,
+          height: 54,
+          decoration: ShapeDecoration(
+            color: Color(0xffF8F8F8),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16))
+          ),
+          child: TextFormField(
+            obscureText: true,
+            controller: _pwdController,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.all(16),
+              hintText: '비밀번호를 입력해 주세요',
+              hintStyle: TextStyle(
+                color: Color(0xff777777),
+                fontWeight: FontWeight.w400,
+                fontSize: 16
+              ),
+            ),
+          ),
+        ),
+        if (_pwdError != null)
+          Padding(
+            padding: EdgeInsets.only(left: 8, top: 4),
+            child: Row(
+              children: [
+                Icon(Icons.error_outline_rounded, size: 12, color: Color(0xffFF4F4F),),
+                SizedBox(width: 2,),
+                Text(_pwdError!, style: TextStyle(
+                    color: Color(0xffFF4F4F),
+                    fontWeight: FontWeight.w600,
+                    fontSize: 12),
+                ),
+              ],
+            ),
+          )
+      ],
+    );
+  }
+
+  // 로그인 버튼
+  Widget loginButton() {
+    return SizedBox(
+        width: SizeConfig.screenWidth * 0.9,
+        height: 57,
+        child: TextButton(onPressed: (){
+          setState(() {
+            _idError = _idController.text.isEmpty ? '아이디를 입력해 주세요' : null;
+            _pwdError = _pwdController.text.isEmpty ? '비밀번호를 입력해 주세요' : null;
+          });
+          if (_idError == null && _pwdError == null) {
+            Navigator.push(context, MaterialPageRoute(
+              builder: (context) {
+                return MenuBottom();
+              },
+            ));
+          }
+        },
+            style: TextButton.styleFrom(
+                backgroundColor: Color(0xffCCE4FF),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16)
+                )
+            ),
+            child: Text('로그인', style: TextStyle(
+                color: Color(0xff0077FF),
+                fontWeight: FontWeight.w600,
+                fontSize: 18
+              ),
+            )
+        )
+    );
+  }
+
+  // 아이디 저장 체크박스
+  Widget checkSaveId() {
+    return Row(
+      children: [
+        Checkbox(
+          activeColor: Colors.transparent,
+          checkColor: Color(0xff777777),
+          value: saveId,
+          onChanged: (value) {
+            setState(() {
+              saveId = value!;
+            });
+          },
+        ),
+        Text('아이디 저장', style: TextStyle(
+            fontWeight: FontWeight.w400,
+            fontSize: 14,
+            color: Color(0xff777777)
+        )
+        )
+      ],
+    );
+  }
+
+  // 회원가입 버튼
+  Widget joinButton() {
+    return SizedBox(
+        width: SizeConfig.screenWidth * 0.9,
+        height: 57,
+        child: TextButton(onPressed: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return JoinScreen();
+          },));
+        },
+            style: TextButton.styleFrom(
+                backgroundColor: Color(0xffE4E4E4),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16)
+                )
+            ),
+            child: Text('회원가입', style: TextStyle(
+                color: Color(0xff777777),
+                fontWeight: FontWeight.w600,
+                fontSize: 18),
+            )
+        )
     );
   }
 }
