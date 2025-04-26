99ff1fe4a1080fbdfd25d9727345ff18566d12d6
SMU-LeeJeongMin <kiposs2@sookmyung.ac.kr>
2025-04-26 11:38:35 +0900

    브랜치 확인용

--- lib/search/search_screen.dart
+++ lib/search/search_screen.dart
@@ -1,9 +1,7 @@
 
 // 검색 메인 화면
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:wanbook/shared/menu_bottom.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
